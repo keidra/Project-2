e8adb1f0bcd80b31b5dd58761560c5247e9cc021
keidra <keidrabaldwin@gmail.com>
2016-04-23 14:15:20 -0700

    Added sign up page functionality. Username and passwords are able to be stored now.

--- static/main.js
+++ static/main.js
@@ -1,1 +1,9 @@
-main.js
+$(function() {
+  // Setup drop down menu
+  $('.dropdown-toggle').dropdown();
+ 
+  // Fix input element click problem
+  $('.dropdown input, .dropdown label').click(function(e) {
+    e.stopPropagation();
+  });
+});
